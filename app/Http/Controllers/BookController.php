0c40d2b9a87f246c28390c0e3f1ade66a1950dd7
Adrian Moldovan <ma.moldovan@gmail.com>
2024-10-14 20:09:36 +0300

    initial commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Books;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index(){
+        $books = Books::all();
+        return response()->json($books);
+    }
+    
+
+    /**
+     * 
+     */
+    public function show($id){
+        $book = Books::find($id);
+        if(empty($book)){
+            return response()->json(["message" => "Book not found"], 404);    
+        }
+
+        return response()->json($book);
+    }
+
+
+    /**
+     * 
+     */
+    public function store(Request $request){
+        $book = new Books;
+        $book->name = $request->name;
+        $book->author = $request->author;
+        $book->publish_date = $request->publish_date;
+        $book->save();
+
+        return response()->json([
+            "message" => "Book added",
+            "book" => $book
+        ], 201);
+    }
+
+
+    /**
+     * 
+     */
+    public function update(Request $request, $id){
+        $book = Books::find($id);
+        if(empty($book)){
+            return response()->json(["message" => "Book not found"], 404);    
+        }
+
+        $book->name = is_null($request->name) ? $book->name : $request->name;
+        $book->author = is_null($request->author) ? $book->author : $request->author;
+        $book->publish_date = is_null($request->publish_date) ? $book->publish_date : $request->publish_date;
+        $book->save();
+
+        return response()->json([
+            "message" => "Book updated",
+            "book" => $book
+        ], 200);
+    }
+
+
+    /**
+     * 
+     */
+    public function destroy($id){
+        $book = Books::find($id);
+        if(empty($book)){
+            return response()->json(["message" => "Book not found"], 404);    
+        }
+
+        $book->delete();
+
+        return response()->json(["message" => "Book deleted"], 204);
+    }
+}
